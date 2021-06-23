877eb3eb248afaffd5d79d88ac84ac05ee12359a
niboo <moonevening8@gmail.com>
2021-06-23 10:29:42 +0900

    background changer

---
+++ index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
+
+function hexCode() {
+	let result = "#";
+	let hexNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+	for (let i = 0; i < 6; i++) {
+		let chosenColor = hexNumber[Math.floor(Math.random() * hexNumber.length)];
+		result += chosenColor;
+	}
+	body.style.backgroundColor = result;
+	h1.textContent = `HEX COLOR: ${result}`;
+
+	btn.addEventListener("submit", hexCode);
+}
+hexCode();
